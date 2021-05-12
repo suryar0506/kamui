95bef93b128d312eb8155e3dd5ef1dfaf808bee6
suryar0506 <72110042+suryar0506@users.noreply.github.com>
2021-05-12 17:56:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>';
+    });
+
+    
+}
+
+console.log("ml5 version:", ml5.version);
+
+function modelLoaded(){
+    console.log("Model is loaded.");
+}
+ classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+
+ function identify_image(){
+     img = document.getElementById("captured_image");
+     classifier.classify(img,gotResult);
+ }
+
+ function gotResult(error,results){
+     if(error){
+         console.error(error);
+     } else {
+         console.log(results);
+         document.getElementById("object_name_display").innerHTML = results[0].label;
+     }
+ }
